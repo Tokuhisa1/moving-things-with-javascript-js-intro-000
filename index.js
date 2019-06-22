6efcd79068ad6a4383bbd83360ebc89ecd5a98fd
Winston Tokuhisa <tokuhisawinston@outlook.com>
2019-06-22 04:01:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ function moveDodgerLeft() {
 }
 
 function moveDodgerRight() {
-  var rightNumbers = dodger.style.right.replace('px', '');
+  var rightNumbers = dodger.style.left.replace('px', '');
   var right = parseInt(rightNumbers, 10);
 
-  if (right < 400) { dodger.style.right = `${right + 1}px`; }
+  if (right < 400) { dodger.style.left = `${right + 1}px`; }
 }
 
 document.addEventListener('keydown', function(e) {
